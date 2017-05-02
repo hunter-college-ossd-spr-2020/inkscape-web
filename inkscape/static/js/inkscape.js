68001cd2fbedd177a69796fb1e35a95d645d4faf
Martin Owens <doctormo@gmail.com>
2017-05-02 14:24:09 -0400

    Move moderation popup to it's own js file

--- inkscape/static/js/inkscape.js
+++ inkscape/static/js/inkscape.js
@@ -205,29 +205,6 @@ function popUpLink(msg, cancel, ok, next, note, ajax_class) {
     a.href = '#' + href
   });
 }
-function popUpModeration(msg, cancel, ok, next, note) {
-  var a = document.currentScript.previousElementSibling;
-  var p = a.parentNode;
-  $(document).ready( function() {
-    var ajax = function(data) {
-      var b = $('.i_voted', p);
-      if(b.length > 0) {
-        b.removeClass('i_voted');
-        var b_vote = $('.'+$(b).attr('class')+'_votes', p);
-        b_vote.text(parseInt(b_vote.text()) - 1);
-      }
-      if(data.weight) {
-        $('.weight', p).text(data.weight);
-      }
-      var a_vote = $('.'+$(a).attr('class')+'_votes', p);
-      a_vote.text(parseInt(a_vote.text()) + 1);
-      $(a).addClass('i_voted');
-    }
-    var href = a.href;
-    $(a).click(function() { return popUp(a.title, msg, href, cancel, ok, next, note, ajax) });
-    a.href = '#' + href;
-  });
-}
 
 function setupMenu() {
   $("#menu-toggle").click(function() {
