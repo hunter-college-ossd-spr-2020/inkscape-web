caa8c58fe6e63005ed8dd2ddac10cac0075daf07
Martin Owens <doctormo@gmail.com>
2017-07-13 20:13:37 -0400

    Double checking resource migrations

---
+++ resources/migrations/0040_auto_20170714_0011.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+import inkscape.fields
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('resources', '0039_auto_20170505_1443'),
+    ]
+
+    operations = [
+        migrations.AlterField(
+            model_name='gallery',
+            name='thumbnail',
+            field=inkscape.fields.ResizedImageField(format=b'PNG', upload_to=b'resources/thumb', max_width=190, min_height=0, max_height=190, blank=True, min_width=0, null=True, verbose_name='Thumbnail'),
+        ),
+        migrations.AlterField(
+            model_name='resource',
+            name='rendering',
+            field=inkscape.fields.ResizedImageField(format=b'PNG', upload_to=b'resources/render', max_width=780, min_height=0, max_height=600, blank=True, min_width=0, null=True, verbose_name='Rendering'),
+        ),
+        migrations.AlterField(
+            model_name='resource',
+            name='thumbnail',
+            field=inkscape.fields.ResizedImageField(format=b'PNG', upload_to=b'resources/thumb', max_width=190, min_height=0, max_height=190, blank=True, min_width=0, null=True, verbose_name='Thumbnail'),
+        ),
+    ]
