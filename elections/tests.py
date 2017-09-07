b287600930679817ca3c3ba3806e3f9ad2d1d051
Martin Owens <doctormo@gmail.com>
2017-09-06 22:50:48 -0400

    Repair test suite for basic testing

--- elections/tests.py
+++ elections/tests.py
@@ -26,7 +26,7 @@ from autotest.base import ExtraTestCase
 from django.core import mail
 from django.utils.timezone import now
 
-from person.models import Team
+from person.models import Team, User
 from .models import Election
 
 class ElectionTests(ExtraTestCase):
@@ -50,37 +50,39 @@ class ElectionTests(ExtraTestCase):
         self.assertContains(response, 'Your Invitation sent to')
         self.assertEqual(len(mail.outbox), 2)
         self.assertEqual(mail.outbox[1].subject, 'Stand for Election: test-election-2011')
-        election._candidates.create(user_id=2, invitor_id=1)
-        election._candidates.create(user_id=3, invitor_id=2)
+        election._candidates.create(user_id=42, invitor_id=45)
+        election._candidates.create(user_id=43, invitor_id=44)
+        election._candidates.create(user_id=44, invitor_id=43)
+        election._candidates.create(user_id=45, invitor_id=42)
 
-        cs = election._candidates.all()
-        response = self.assertGet('elections:accept-yes', team='e_team', slug='test-election-2011', hash=cs[0].slug)
+        cs = election._candidates.order_by('id').values_list('slug', flat=True)
+        response = self.assertGet('elections:accept-yes', team='e_team', slug='test-election-2011', hash=cs[0])
         self.assertContains(response, 'Invitation Accepted')
+        self.assertEqual(election.candidates.count(), 1)
 
         # Check Invitations lists
-        self.assertContains(response, 'Testing Staff')
-
-        # XXX Votes can NOT be submitted
+        self.assertContains(response, 'Testing Admin')
 
         # Change election to invitations sent
         election.invitation_close()
 
-        response = self.assertGet('elections:accept-yes', team='e_team', slug='test-election-2011', hash=cs[1].slug)
-        self.assertContains(response, 'Invitation Accepted')
+        response = self.assertGet('elections:accept-yes', team='e_team', slug='test-election-2011', hash=cs[1])
+        self.assertEqual(election.candidates.count(), 2)
+
+        response = self.assertGet('elections:accept-yes', team='e_team', slug='test-election-2011', hash=cs[2])
+        self.assertEqual(election.candidates.count(), 3)
 
         # Reject invitation
-        response = self.assertGet('elections:accept-no', team='e_team', slug='test-election-2011', hash=cs[2].slug)
+        response = self.assertGet('elections:accept-no', team='e_team', slug='test-election-2011', hash=cs[3])
         self.assertContains(response, 'Invitation NOT Accepted')
-
-        # XXX Votes can NOT be submitted
-        # XXX New invitation can be added, but doesn't matter
+        self.assertEqual(election.candidates.count(), 3)
 
         # No ballots yet created
         self.assertEqual(election.ballots.count(), 0)
 
         # Change election to voting mode, email to voting team is sent
         election.voting_open()
-        self.assertEqual(len(mail.outbox), 5)
+        self.assertEqual(len(mail.outbox), 3)
         self.assertEqual(mail.outbox[-1].subject, 'Election King: Voting Open')
 
         # Ballots now exist
@@ -91,25 +93,37 @@ class ElectionTests(ExtraTestCase):
                 slug='test-election-2011', hash=ballot.slug, data=votes)
             self.assertContains(response, 'ballot has been saved')
 
-        # XXX Votes can be submitted
-        # XXX invitations and invitation acceptions don't matter
-        # XXX Votes can be ranked
-
         # Change election to finish mode, email to voting team is sent with results
         election.voting_close()
-        self.assertEqual(len(mail.outbox), 6)
+        self.assertEqual(len(mail.outbox), 4)
         self.assertEqual(mail.outbox[-1].subject, 'Election King: Results')
 
-        # Log contains results
-        self.assertEqual(election.log, '{...}')
+        log = election.get_log
+        # Log contains an election type meta documentation
+        self.assertIn('type', log)
 
+        # All the counts for various things
+        self.assertEqual(log['counts']['ignored'], 1)
+        self.assertEqual(log['counts']['voters'], 5)
+        self.assertEqual(log['counts']['rejected'], 1)
+        self.assertEqual(log['counts']['candidates'], 3)
+        self.assertEqual(log['counts']['ballots'], 5)
+        self.assertEqual(log['counts']['invites'], 5)
+
+        # The actual votes
+        self.assertEqual(len(log['votes']), 5)
+        self.assertEqual(set(log['votes'][0]['paper']), set([1, 42, 43]))
+        self.assertEqual(log['votes'][0]['user_id'], 45)
+
+        # Candidate objects
+        self.assertTrue(isinstance(log['candidates'][0], User))
+
+        # Election Results
+        self.assertEqual(log['results']['winners'], [1])
+        self.assertEqual(len(log['results']['rounds']), 1)
         # Votes and candidate lists are cleared
-        self.assertEqual(election._candidates.count, 0)
-        self.assertEqual(election.ballots.count, 0)
-
-        # XXX Votes can NOT be submitted
-
-
+        self.assertEqual(election._candidates.count(), 0)
+        self.assertEqual(election.ballots.count(), 0)
 
 
 
