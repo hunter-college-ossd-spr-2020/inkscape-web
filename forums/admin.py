9e39c96b740c206d518db3ca6cf94d0eff58dffc
Martin Owens <doctormo@gmail.com>
2018-11-16 13:41:08 -0500

    Add admin interface for comment flags

--- forums/admin.py
+++ forums/admin.py
@@ -1,7 +1,7 @@
 #
 # Copyright 2016, Martin Owens <doctormo@gmail.com>
 #
-# This file is part of the software inkscape-web, consisting of custom 
+# This file is part of the software inkscape-web, consisting of custom
 # code for the Inkscape project's django-based website.
 #
 # inkscape-web is free software: you can redistribute it and/or modify
@@ -17,11 +17,24 @@
 # You should have received a copy of the GNU Affero General Public License
 # along with inkscape-web.  If not, see <http://www.gnu.org/licenses/>.
 #
-from django.contrib.admin import ModelAdmin, site
+"""
+Admin interfaces for comments and forums.
+"""
 
-from .models import *
+from django.contrib.admin import ModelAdmin, site, TabularInline
+
+from .models import ForumGroup, Forum, ForumTopic
 
 site.register(ForumGroup)
 site.register(Forum)
 site.register(ForumTopic)
 
+from django_comments.models import CommentFlag
+from django_comments.admin import CommentsAdmin
+
+class FlagInline(TabularInline):
+    raw_id_fields = ('user',)
+    model = CommentFlag
+    extra = 0
+
+CommentsAdmin.inlines = (FlagInline,)
