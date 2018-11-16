371eae090649edb5bf95442fc7f2ea0de24aee91
Martin Owens <doctormo@gmail.com>
2018-11-16 13:42:55 -0500

    Add emoji with text based and image based support based on CommentFlags

--- forums/static/forums/js/has_changed.js
+++ forums/static/forums/js/has_changed.js
@@ -23,4 +23,29 @@ $(document).ready(function() {
         target.addClass("changed");
     }
   });
+
+  var static_dir = $('body').data('static');
+  /* Emojis */
+  $('span.emoji').each(function() {
+    var emoji = $(this).text();
+    if(emoji) {
+        $(this).empty();
+        var code = emoji.charCodeAt(0).toString(16);
+        var sibling = $('.code-' + code, $(this).parent());
+
+        if(sibling.length) {
+            // Count number of emojis of the same kind
+            $(this).remove();
+            var count = sibling.data('count') + 1;
+            sibling.data('count', count);
+            $('i', sibling).text(count).show();
+        } else {
+            // Create a new emoji of the right kind
+            $(this).data('count', 1);
+            $(this).addClass('code-'+code);
+            $(this).append($('<img src="'+static_dir+'/emoji/48/'+code+'.png" alt="'+emoji+'" class="emoji">'));
+            $(this).append($('<i class="count" style="display: none;">1</i>'));
+        }
+    }
+  });
 });
