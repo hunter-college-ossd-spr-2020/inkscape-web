cb253656ecbb111024952bfe8404b75af4a15b27
Martin Owens <doctormo@gmail.com>
2018-11-19 12:24:38 -0500

    Reformat Topic creation to fit new style

--- forums/forms.py
+++ forums/forms.py
@@ -1,5 +1,5 @@
 #
-# Copyright 2016, Martin Owens <doctormo@gmail.com>
+# Copyright 2016-2018, Martin Owens <doctormo@gmail.com>
 #
 # This file is part of the software inkscape-web, consisting of custom
 # code for the Inkscape project's django-based website.
@@ -24,11 +24,11 @@ I can't believe I had to re-write this after copying over it.
 from django.utils.translation import ugettext_lazy as _
 from django.utils.timezone import now
 
-from django.forms import *
-from django_comments.forms import CommentForm, ContentType, ErrorDict
+from django.forms import CharField, ValidationError
+from django_comments.forms import CommentForm, ContentType, ErrorDict, COMMENT_MAX_LENGTH
 from djangocms_text_ckeditor.widgets import TextEditorWidget
 
-from .models import Forum, ForumTopic
+from .models import ForumTopic
 
 class AddCommentForm(CommentForm):
     """This CommentForm replaces the django_comments one, what it provides is
@@ -40,12 +40,12 @@ class AddCommentForm(CommentForm):
 
     def is_locked(self, **kw):
         """Return true if this configured comment form is locked"""
-        pk = kw.get('object_pk', self.initial['object_pk'])
+        pkey = kw.get('object_pk', self.initial['object_pk'])
         if isinstance(self.target_object, ForumTopic):
-            topics = ForumTopic.objects.filter(pk=pk)
+            topics = ForumTopic.objects.filter(pk=pkey)
         else:
-            ct = ContentType.objects.get_for_model(self.target_object)
-            topics = ForumTopic.objects.filter(object_pk=pk, forum__content_type_id=ct.pk)
+            ctype = ContentType.objects.get_for_model(self.target_object).pk
+            topics = ForumTopic.objects.filter(object_pk=pkey, forum__content_type_id=ctype)
         return any(topics.values_list('locked', flat=True))
 
     def clean_object_pk(self):
@@ -57,36 +57,47 @@ class AddCommentForm(CommentForm):
     def security_errors(self):
         """Return just those errors associated with security"""
         errors = ErrorDict()
-        for f in ["honeypot", "timestamp", "security_hash", "object_pk"]:
-            if f in self.errors:
-                errors[f] = self.errors[f]
+        for field in ["honeypot", "timestamp", "security_hash", "object_pk"]:
+            if field in self.errors:
+                errors[field] = self.errors[field]
         return errors
 
 
 
 class NewTopicForm(CommentForm):
+    """
+    Show a new Topic forum for creating new forum topics.
+    """
     subject = CharField()
+    # Remove fields from CommentForm, Meta exclude doesn't work
+    # because this isn't a ModelForm, but a bog standard Form
+    url = None
     name = None
     email = None
+    # Use CKEditor Text Widget for the comment field
+    comment = CharField(label=_('Comment'),\
+        widget=TextEditorWidget(configuration='CKEDITOR_FORUM'),\
+        max_length=COMMENT_MAX_LENGTH)
+    field_order = ('subject', 'comment', 'honeypot')
 
     def __init__(self, user, ip_address, *args, **kwargs):
         self.user = user
-        self.ip = ip_address
+        self.ip_address = ip_address
         super(NewTopicForm, self).__init__(*args, **kwargs)
 
-    def save(self, **kw):
+    def save(self, **_):
+        """Save the comment under a topic's object"""
         subject = self.cleaned_data['subject']
         self.target_object = self.target_object.topics.create(
-                subject=subject, last_posted=now())
+            subject=subject, last_posted=now())
 
         self.cleaned_data['name'] = self.user.username
         self.cleaned_data['email'] = self.user.email
+        self.cleaned_data['url'] = ''
 
         comment = self.get_comment_object()
         comment.user = self.user
-        comment.ip_address = self.ip
+        comment.ip_address = self.ip_address
         comment.save()
 
         return self.target_object
-        
-
