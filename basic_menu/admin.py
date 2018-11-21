3de8faca5de1f24ba5d0338fbe5f2f7dad858de6
Jabiertxof <jabier.arraiza@marker.es>
2018-11-21 13:26:44 +0100

    remove remainig code in template

--- basic_menu/admin.py
+++ basic_menu/admin.py
@@ -18,22 +18,23 @@
 # You should have received a copy of the GNU Affero General Public License
 # along with inkscape-web.  If not, see <http://www.gnu.org/licenses/>.
 #
-from django.conf import settings
-from django.conf.urls import url
-from django.core.cache import cache
 from django.http import HttpResponseRedirect
 from django.contrib.admin import ModelAdmin, TabularInline, site
+from django.conf.urls import include, url
 from cms.models.pagemodel import Page
-from menus.menu_pool import menu_pool
-from .views import MenuShow
+from django.utils.translation import ugettext_lazy as _
+from django.conf import settings
 from .models import MenuItem, MenuRoot
 
-class MenuItemsInline(TabularInline):
+class NestableTabularInline(TabularInline):
+    class Media:
+        js = ('js/jquery.nestable.js', 'js/admin.nestable.js')
+
+
+class MenuItemsInline(NestableTabularInline):
     """Show MenuItems in a stacked tab interface"""
     model = MenuItem
     extra = 1
-
-
 
 class MenuRootAdmin(ModelAdmin):
     """Customise the root menu in the admin interface"""
@@ -50,11 +51,42 @@ class MenuRootAdmin(ModelAdmin):
 
     def populize_menu(self, request):
         #TODO: clear all
+        MenuItem.objects.all().delete()
+        MenuRoot.objects.all().delete()
         for language  in settings.LANGUAGES :
-            menu_show = MenuShow(language, request);
-            cache.delete(menu_show.cache_key)
-            menu = menu_show.populize_lang()
-        return HttpResponseRedirect('/admin/basic_menu/menuroot/')
-        
-site.register(MenuRoot, MenuRootAdmin)
+            lang = language[0]
+            root = MenuRoot(lang)
+            root.save()
+            cms_pages = Page.objects.public()
+            counter = 0
+            items = []
+            for page in cms_pages:
+                    counter += 1
+                    items['parent'] = page.parent_id
+                    items['id'] = page.id
+                    items['url'] = page.get_absolute_url(lang)
+                    items['name'] = page.get_menu_title(lang)
+                    items['orden'] = counter
+            root_counter = 0
+            for item in items.filter(item.parent = 0):
+                root_counter += 1
+                menuitem = MenuItem()
+                menuitem.parent = None
+                menuitem.url = item.url
+                menuitem.name = item.name
+                menuitem.orden = root_counter
+                menuitem.root = root
+                menuitem.save()
+                child_counter = 0
+                for subitem in items.filter(item.parent = item.id):
+                        child_counter += 1
+                        submenuitem = MenuItem()
+                        submenuitem.parent = menuitem.id
+                        submenuitem.url = subitem.url
+                        submenuitem.name = subitem.name
+                        submenuitem.orden = child_counter
+                        submenuitem.root = root
+                        submenuitem.save()
+        return HttpResponseRedirect('/admin/basic_menu/')
 
+site.register(MenuRoot, MenuRootAdmin)
