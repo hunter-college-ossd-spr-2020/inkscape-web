4f27dc81973c4b5d150dd73ef7c408e8a057b16b
Martin Owens <doctormo@gmail.com>
2018-12-01 12:43:28 -0500

    Add forum icons to fixtures

--- resources/management/commands/loaddata.py
+++ resources/management/commands/loaddata.py
@@ -3,7 +3,7 @@
 #
 # Copyright 2015, Martin Owens <doctormo@gmail.com>
 #
-# This file is part of the software inkscape-web, consisting of custom 
+# This file is part of the software inkscape-web, consisting of custom
 # code for the Inkscape project's django-based website.
 #
 # inkscape-web is free software: you can redistribute it and/or modify
@@ -26,7 +26,7 @@ These are collections of files ready to be used with database entries
 for example FileField and ImageField.
 """
 
-from os.path import dirname, exists, isdir, join, relpath
+import os
 
 from django.conf import settings
 import django.core.management.commands.loaddata
@@ -37,19 +37,25 @@ from django.db.models.fields.files import FileField
 from django.utils._os import upath
 from django.apps import apps
 
-# For Python < 3.3
-file_not_found_error = getattr(__builtins__,'FileNotFoundError', IOError)
-
 
 def models_with_filefields():
     for klass in apps.get_models():
         if any(isinstance(field, FileField) for field in klass._meta.fields):
             yield klass
 
+def find_fixture_paths():
+    """Return the full paths to all possible fixture directories."""
+    app_module_paths = []
+    for app in apps.get_app_configs():
+
+        app_module_paths.append(upath(app.path))
+
+    app_fixtures = [os.path.join(path, 'fixtures') for path in app_module_paths]
+    return app_fixtures + list(settings.FIXTURE_DIRS)
+
 GLOBAL_WARN = set()
 
 class Command(django.core.management.commands.loaddata.Command):
-
     def load_images_for_signal(self, sender, instance, **kwargs):
         not_found = set()
         is_found  = set()
@@ -61,19 +67,19 @@ class Command(django.core.management.commands.loaddata.Command):
             if path is None or not path.name:
                 continue
             for fixture_path in self.fixture_media_paths:
-                filepath = join(fixture_path, path.name)
+                filepath = os.path.join(fixture_path, path.name)
                 try:
-                    with open(filepath, 'rb') as f:
-                        default_storage.save(path.name, f)
+                    with open(filepath, 'rb') as fhl:
+                        default_storage.save(path.name, fhl)
                         is_found.add(path.name)
                         break
-                except file_not_found_error:
+                except FileNotFoundError:
                     not_found.add(path.name)
 
         for filename in (not_found - is_found):
             if filename not in GLOBAL_WARN: # _IsWarning_
                 if filename[0] != '/':
-                    self.stderr.write(" [skipping] Expected file: %s" % filepath)
+                    self.stderr.write(" [skipping] Expected file: %s" % filename)
                 GLOBAL_WARN.add(filename)
 
     def handle(self, *fixture_labels, **options):
@@ -81,9 +87,9 @@ class Command(django.core.management.commands.loaddata.Command):
         for klass in models_with_filefields():
             signals.pre_save.connect(self.load_images_for_signal, sender=klass)
 
-        fixture_paths = self.find_fixture_paths()
-        fixture_paths = (join(path, 'media') for path in fixture_paths)
-        fixture_paths = [path for path in fixture_paths if isdir(path)]
+        fixture_paths = find_fixture_paths()
+        fixture_paths = (os.path.join(path, 'media') for path in fixture_paths)
+        fixture_paths = [path for path in fixture_paths if os.path.isdir(path)]
         self.fixture_media_paths = fixture_paths
 
         ret = super(Command, self).handle(*fixture_labels, **options)
@@ -93,14 +99,3 @@ class Command(django.core.management.commands.loaddata.Command):
             signals.pre_save.disconnect(self.load_images_for_signal, sender=klass)
 
         return ret
-
-    def find_fixture_paths(self):
-        """Return the full paths to all possible fixture directories."""
-        app_module_paths = []
-        for app in apps.get_app_configs():
-
-            app_module_paths.append(upath(app.path))
-
-        app_fixtures = [join(path, 'fixtures') for path in app_module_paths]
-
-        return app_fixtures + list(settings.FIXTURE_DIRS)
