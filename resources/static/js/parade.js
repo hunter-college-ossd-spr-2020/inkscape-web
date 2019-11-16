ed1a6c817c93aa1f02bf32640c582aecb8f542f4
Martin Owens <doctormo@gmail.com>
2019-11-16 11:52:06 -0500

    Updated jabier's parade example 1.0.8

--- resources/static/js/parade.js
+++ resources/static/js/parade.js
@@ -1,10 +1,12 @@
 var prevpos = 0;
 
 $("body").on("click", ".sendcomment", function (e) {
+  if (!$("#wrapper").hasClass("insensitive")) {
     e.preventDefault();
     var win = window.open($(this).attr("href"), '_blank');
     win.focus();
-})
+  }
+});
 
 $("body").on("click", ".tooltip", function () {
   if (!$("#wrapper").hasClass("insensitive")) {
@@ -13,6 +15,7 @@ $("body").on("click", ".tooltip", function () {
     var factor = 750/625;
     $("#wrapper").css("height",$(window).height() - 40);
     $("#wrapper").addClass("insensitive");
+    $("#wrapper").addClass("loading");
     $("body").addClass("open");
     $("#grid").css("opacity", "0");
     $("html").css("background-color","#fff");
@@ -20,6 +23,7 @@ $("body").on("click", ".tooltip", function () {
     var downloadimage = $('<img>');
     downloadimage.on("load", function () {
       $("#wrapper").css("background-image","url(" + $(this).attr("src") + ")");
+      $("#wrapper").removeClass("loading");
     });
     downloadimage.attr("src", $(this).prev().data("rendering"));
   } 
@@ -31,6 +35,7 @@ $("body").on("click", ".insensitive", function () {
   $("#wrapper").css("min-height",$(window).height() - 40);
   //$("#wrapper").css("min-height",$("body").height() / factor);
   $("#wrapper").removeClass("insensitive");
+  $("#wrapper").removeClass("loading");
   $("body").removeClass("open");
   $("#grid").css("opacity", "1");
   $("html").css("background-color","#000");
