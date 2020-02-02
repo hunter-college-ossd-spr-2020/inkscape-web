8365dda7af45d9a2f49eaf6960ea7dbe709dde11
kmAyush <kmayush27399@gmail.com>
2020-02-02 11:33:56 +0530

    Add codesnippet plugin to config.js

--- forums/static/forums/ckeditor/config.js
+++ forums/static/forums/ckeditor/config.js
@@ -1,5 +1,6 @@
 CKEDITOR.editorConfig = function( config ) {
         config.plugins = 'basicstyles,dialogui,dialog,notification,button,toolbar,clipboard,enterkey,entities,floatingspace,wysiwygarea,indent,indentlist,fakeobjects,link,list,undo,youtube,textwatcher,autocomplete,textmatch,xml,ajax,mentions,panelbutton,panel,floatpanel,emoji,listblock,richcombo,format,codeTag,blackquote,colorbutton,autolink,maximize';
+        config.extraPlugins = 'codesnippet';
 	config.toolbarGroups = [
 		{ name: 'document', groups: [ 'mode', 'document', 'doctools' ] },
 		{ name: 'clipboard', groups: [ 'clipboard', 'undo' ] },
@@ -13,7 +14,8 @@ CKEDITOR.editorConfig = function( config ) {
 		{ name: 'colors', groups: [ 'colors' ] },
 		{ name: 'tools', groups: [ 'tools' ] },
 		{ name: 'others', groups: [ 'others' ] },
-		{ name: 'about', groups: [ 'about' ] }
+		{ name: 'about', groups: [ 'about' ] },
+		{ name: 'codesnippet', groups: [ 'codesnippet' ] },
 	];
 
 	config.removeButtons = 'Undo,Redo,Anchor,Subscript,Superscript';
